179ee45bbd0c6926143e72251d329adeaf1ea9a5
wangyue <sean@portkey.finance>
2023-07-17 15:05:03 +0800

    feat:add Twilio

---
+++ src/CAVerifierServer.Application/Phone/TwilioSmsMessageSender.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CAVerifierServer.CustomException;
+using CAVerifierServer.Options;
+using CAVerifierServer.VerifyCodeSender;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Twilio;
+using Twilio.Rest.Verify.V2.Service;
+using Volo.Abp.Sms;
+
+namespace CAVerifierServer.Phone;
+
+public class TwilioSmsMessageSender : ISMSServiceSender
+{
+    public string ServiceName => "Twilio";
+    private readonly ILogger<AwsSmsMessageSender> _logger;
+    private readonly VerifierInfoOptions _verifierInfoOptions;
+    private readonly TwilioSmsMessageOptions _twilioSmsMessageOptions;
+    private const string PhoneNumReplacement = "$1****$2";
+    private readonly Regex _regex;
+    private const string CNCode = "+86";
+    private const string CustomerVar = "custom_var";
+    private const string VerifyStatus = "pending";
+
+    public TwilioSmsMessageSender(ILogger<AwsSmsMessageSender> logger,
+        IOptions<VerifierInfoOptions> verifierInfoOptions,
+        IOptions<TwilioSmsMessageOptions> twilioSmsMessageOptions)
+    {
+        _logger = logger;
+        _regex = new Regex("(.{6}).*(.{4})");
+        _twilioSmsMessageOptions = twilioSmsMessageOptions.Value;
+        _verifierInfoOptions = verifierInfoOptions.Value;
+    }
+
+
+    public async Task SendAsync(SmsMessage smsMessage)
+    {
+        if (string.IsNullOrEmpty(smsMessage.PhoneNumber) || string.IsNullOrEmpty(smsMessage.Text))
+        {
+            _logger.LogError("PhoneNum or message text is invalidate");
+            return;
+        }
+
+        var customSubstitutions = new Dictionary<string, string> { { CustomerVar, _verifierInfoOptions.Name } };
+        var customSubstitutionsJsonStr = JsonConvert.SerializeObject(customSubstitutions);
+
+        try
+        {
+            TwilioClient.Init(_twilioSmsMessageOptions.AccountSid, _twilioSmsMessageOptions.AuthToken);
+            VerificationResource verification;
+            if (smsMessage.PhoneNumber.StartsWith(CNCode))
+            {
+                verification = await VerificationResource.CreateAsync(
+                    to: smsMessage.PhoneNumber,
+                    locale: _twilioSmsMessageOptions.Locale,
+                    customCode: smsMessage.Text,
+                    templateCustomSubstitutions: customSubstitutionsJsonStr,
+                    channel: _twilioSmsMessageOptions.Channel,
+                    pathServiceSid: _twilioSmsMessageOptions.ServiceId
+                );
+            }
+            else
+            {
+                verification = await VerificationResource.CreateAsync(
+                    to: smsMessage.PhoneNumber,
+                    templateSid: _twilioSmsMessageOptions.TemplateId,
+                    locale: _twilioSmsMessageOptions.Locale,
+                    customCode: smsMessage.Text,
+                    templateCustomSubstitutions: customSubstitutionsJsonStr,
+                    channel: _twilioSmsMessageOptions.Channel,
+                    pathServiceSid: _twilioSmsMessageOptions.ServiceId
+                );
+            }
+
+
+            if (verification.Status != VerifyStatus)
+            {
+                _logger.LogError(
+                    "Twilio SMS Service sending SMSMessage failed to {phoneNum}, reason is {errorMessage}",
+                    _regex.Replace(smsMessage.PhoneNumber, PhoneNumReplacement), verification.DateUpdated);
+                throw new SmsSenderFailedException("Twilio SMS Service sending SMSMessage failed");
+            }
+
+            _logger.LogDebug("Twilio SMS Service sending SMSMessage to {phoneNum}",
+                _regex.Replace(smsMessage.PhoneNumber, PhoneNumReplacement));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Twilio SMS Service Sending message error : {ex}", ex.Message);
+            throw ex;
+        }
+    }
+}
